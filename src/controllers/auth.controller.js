5c0510297a130d62e2db7e3b5dca0c127d8323a2
akashroy18 <akash02052005@gmail.com>
2026-04-04 11:37:54 +0530

    auth part done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,3 +32,51 @@ export const registerUser = async (req,res)=>{
     })
 }
 }
+export const login = async(req,res)=>{
+    try {
+        const {email,password}= req.body
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                message:"USer not exsist,register first"
+            })
+        }
+        const isPassword = await bcrypt.compare(password,user.password)
+        if(!isPassword){
+            return res.status(400).json({
+                message:"Incorrect Password"
+            })
+        }
+        const token = jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        )
+        res.cookie("token",token)
+        res.status(200).json({
+            message:"User logged in Succsefully",
+            success:true
+        })
+    } catch (error) {
+        console.log("error in log in")
+        res.status(500).json({ message: error.message })
+    }
+}
+export const logout = async (req,res)=>{
+    try {
+        const token = req.cookies.token
+        if(!token){
+            return res.status(400).json({
+                message:"User alread logged out"
+            })
+        }
+        res.clearCookie("token")
+        res.status(200).json({
+        success: true,
+        message: "Logged out successfully"
+        })
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 import express from 'express'
-import { registerUser } from '../controllers/auth.controller.js'
+import { registerUser,login ,logout} from '../controllers/auth.controller.js'
 const router = express.Router()
 router.post("/register",registerUser)
+router.post("/login",login)
+router.get("/logout",logout)
 export default router
